16a02cd6b496bff30f10caa274a68ffefacfe45f
Aritra Biswas <aritrabb@gmail.com>
2025-07-06 04:12:05 +0200

    Complete exercise 1.12 - Add image storage cache for The Project

--- the-project/todo-list-app.py
+++ the-project/todo-list-app.py
@@ -1,16 +1,52 @@
 import os
-from http.server import SimpleHTTPRequestHandler, HTTPServer
+import requests
+import time
+from flask import Flask, render_template, url_for
 
-SERVERPORT = int(os.environ.get('PORT', 8080))
+app = Flask(__name__)
 
+start = time.time()
+
+first_req = True
+
+dirname = os.path.dirname(__file__)
+
+@app.route('/')
 def main():
-    httpd = HTTPServer(('', SERVERPORT), SimpleHTTPRequestHandler)
+    global start
+    global first_req
 
-    print(f"Started server in port {SERVERPORT}", flush=True)
+    last_refreshed = time.time()
+    elapsed = abs(start - last_refreshed)
 
-    httpd.serve_forever()
+    if elapsed > 600 or first_req:
+        first_req = False
+        response = requests.get("https://picsum.photos/1200")
+
+        if response.status_code == 200:
+            # change when deploying in k8s
+            with open(os.path.join(dirname, "static/image.jpg"), "wb") as fileptr:
+                fileptr.write(response.content)
+
+        start = last_refreshed
+
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    main()
+    app.run("0.0.0.0", port=os.environ.get("PORT", 8080))
 
+# import os
+# from http.server import SimpleHTTPRequestHandler, HTTPServer
+#
+# SERVERPORT = int(os.environ.get('PORT', 8080))
+#
+# def main():
+#     httpd = HTTPServer(('', SERVERPORT), SimpleHTTPRequestHandler)
+#
+#     print(f"Started server in port {SERVERPORT}", flush=True)
+#
+#     httpd.serve_forever()
+#
+# if __name__ == '__main__':
+#     main()
 
